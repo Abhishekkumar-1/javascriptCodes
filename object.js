a544fa2cf2ab5bae5dc6314908c6841e2888fda5
Abhishekkumar-1 <abhisheksniperstar@gmail.com>
2024-01-06 16:01:38 +0530

    Class methods and instances

--- object.js
+++ object.js
@@ -2,21 +2,31 @@ Date.prototype.shortFormat=function(){
     return (this.getMonth()+1)+"/"+this.getDate()+"/"+this.getFullYear();
 }
 
-function Blog(body,date){
+function Blog(body,date,image){
     this.body=body
     this.date=date
+    this.image=image
     Blog.prototype.signature="by Abhishek Kumar"
 
     Blog.prototype.toHTML=function(i){
         var blogText="";
-        blogText+=i?"<p style='background-color:#EEEEEE'>":"<p>";
-        blogText+="<strong>"+(this.date.shortFormat())+"</strong><br/>"+this.body+"</br><em style='font-size:12px'>"+this.signature+"</em></p>";
+        if(this.image){
+            blogText+=i?"<p style='background-color:#EEEEEE'>":"<p>";
+            blogText+="<strong>"+(this.date.shortFormat())+"</strong><br/><table><tr><td><img src='./assets/"+this.image+"'/></td><td style='vertical-align:top'>"+this.body+"</td></tr></table><em style='font-size:12px'>"+this.signature+"</em></p>";
+        }else{
+            blogText+=i?"<p style='background-color:#EEEEEE'>":"<p>";
+            blogText+="<strong>"+(this.date.shortFormat())+"</strong><br/>"+this.body+"</br><em style='font-size:12px'>"+this.signature+"</em></p>";
+        }
 
         return blogText;
     }
 
     Blog.prototype.searchText=function(text){
         return this.body.toLowerCase().indexOf(text.toLowerCase())!=-1;
+    }
+
+    Blog.blogSorter=function(blog1,blog2){
+        return blog2.date-blog1.date
     }
 
 
@@ -28,19 +38,17 @@ var blogs=[
     new Blog('I got an new cube as my award',new Date('08/08/2016')),
     new Blog('I saw a new type of cube...',new Date('07/21/2019')),
     new Blog('I solved a 3*3 rubicks cube ',new Date('04/27/2019')),
-    new Blog('I ordered a new cube ...',new Date('08/05/2020')),
-    new Blog("Got the new cube I ordered. It's a real pearl.", new Date("08/14/2008")),
+    new Blog('I ordered a new cube ...',new Date('08/05/2020'),"cube777.png"),
+    new Blog("Got the new cube I ordered. It's a real pearl.", new Date("08/14/2008"),"cube778.png"),
     new Blog("Solved the new cube but of course, now I'm bored and shopping for a new one.", new Date("08/19/2008")),
     new Blog("Managed to get a headache toiling over the new cube. Gotta nap.", new Date("08/16/2008")),
     new Blog("Found a 7x7x7 cube for sale online. Yikes! That one could be a beast.", new Date("08/21/2008")),
-    new Blog("Met up with some fellow cubers to discuss the prospect of a 7x7x7 cube. Mixed feelings.", new Date("08/29/2008")),
+    new Blog("Met up with some fellow cubers to discuss the prospect of a 7x7x7 cube. Mixed feelings.", new Date("08/29/2008"),"cube779.jpg"),
     new Blog("Went ahead and ordered the scary 7x7x7 cube. Starting a mental exercise regimen to prepare.", new Date("09/01/2008")),
     new Blog("Attended a rally outside of a local toy store that stopped carrying cube puzzles. Power to the puzzlers!", new Date("09/03/2008"))
 ]
 
-blogs.sort(function (blog1,blog2){
-    return blog2.date-blog1.date;
-})
+blogs.sort(Blog.blogSorter)
 
 function showBlog(numEntries){
     if(!numEntries){
